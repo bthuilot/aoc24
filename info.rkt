b51c358d458f72f791f6880bd9d3b4927c0889bd
Bryce Thuilot <bryce@thuilot.io>
2024-11-09 15:51:55 -0500

    fix: version number
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
 (define build-deps '("racket-doc" "rackunit-lib"))
 (define scribblings '()) ;'(("scribblings/24.scrbl" ())))
 (define pkg-desc "Advent of Code 2024.")
-(define version "0.0.0")
+(define version "0.0")
 (define pkg-authors '(Bryce Thuilot <bryce@thuilot.io>))
 (define license '(GPL-3.0))
