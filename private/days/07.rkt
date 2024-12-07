24df2411ebe28c44356668230e2a8354ee8bace6
Bryce Thuilot <bryce@thuilot.io>
2024-12-06 22:01:23 -0500

    chore(day 7): add scaffolding
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

---
+++ private/days/07.rkt
@@ -0,0 +1,22 @@
+#|
+
+Day 7
+https://adventofcode.com/2024/day/07
+
+|#
+
+#lang racket/base
+
+
+(require "../solution.rkt")
+
+(provide run)
+
+(define (run input)
+  (let ([part1 null]
+        [part2 null])
+    null))
+
+
+;; (module+ test
+;;  (require rackunit))
